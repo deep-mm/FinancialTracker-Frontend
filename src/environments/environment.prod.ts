a47695860e65f7c93bfa3564b061e3c35797ec86
deep-mm <deep.mm@somaiya.edu>
2021-12-31 13:16:37 +0530

    Update environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://financetracker-api.azurewebsites.net/api'
+  apiBaseUrl: 'https://financetracker-api.azurewebsites.net/api',
+  scopeUri: ['api://28c9b16a-b573-4c7c-bb52-63c3e0a435f2/access_as_user'],  
+  tenantId: 'cfe5e889-6502-44a1-9d56-9c70e5543454',
+  uiClienId: '28c9b16a-b573-4c7c-bb52-63c3e0a435f2',
+  redirectUrl: 'https://financetracker-app.azurewebsites.net'
 };
